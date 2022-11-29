e68b9a6be7e76474faaef7cad6dbaf1fbc2b49a4
Arya Soni <aryasoni98@gmail.com>
2022-11-29 22:21:16 +0530

    Bug

--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,8 @@ module.exports = {
   plugins: [
     ['@babel/plugin-proposal-private-methods', { loose: true }],
     ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
     [
       'module-resolver',
       {
